df884fa5d5baa2766f09dc8b11960b9207b529a4
JunHoo Lee <mrjunoo@snu.ac.kr>
2023-01-16 18:51:44 +0900

    a

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python3 main.py -train \
+CUDA_VISIBLE_DEVICES=0 python3 main.py -train \
 		-verbose \
 		-model_dir train_model \
 		-N 5 \
 		-K 1 \
 		-embedding_dir /data/embeddings/ \
 		-dataset miniImageNet \
-		-exp_name toy-0511 \
+		-exp_name leo_pure \
         -save_best \
 		-save_checkpoint 
 		
