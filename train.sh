49fef43bde24266b4eec3cc58284231af70c320f
JunHoo Lee <mrjunoo@snu.ac.kr>
2023-01-16 15:54:27 +0900

    change datadir

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ python3 main.py -train \
 		-model_dir train_model \
 		-N 5 \
 		-K 1 \
-		-embedding_dir ../embeddings/ \
+		-embedding_dir /data/embeddings/ \
 		-dataset miniImageNet \
 		-exp_name toy-0511 \
 		-save_checkpoint 
