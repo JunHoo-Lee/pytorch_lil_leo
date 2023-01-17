58fa50f27f86fcffba796a683e20eec6d18ff275
JunHoo Lee <mrjunoo@snu.ac.kr>
2023-01-17 11:44:23 +0900

    aP

--- test1.sh
+++ test1.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0 python3 main.py -test \
-        -verbose \
-        -model_dir train \
-        -N 5 \
-        -K 1 \
-        -embedding_dir /data/embeddings/ \
-        -dataset miniImageNet \
-        -exp_name leo_pure \
-        -load train_model/miniImageNet_1shot/leo_pure/best_model.pt
+    -verbose \
+    -model_dir train \
+    -N 5 \
+    -K 1 \
+    -embedding_dir /data/embeddings/ \
+    -dataset miniImageNet \
+    -exp_name leo_pure \
+    -load train_model/miniImageNet_1shot/leo_pure/best_model.pt
+echo "leo_pure"
 
 CUDA_VISIBLE_DEVICES=0 python3 main.py -test \
-        -verbose \
-        -model_dir train \
-        -N 5 \
-        -K 1 \
-        -embedding_dir /data/embeddings/ \
-        -dataset miniImageNet \
-        -exp_name leo_onesteplr_0.01_linearity_0.00 \
-        -load train_model/miniImageNet_1shot/leo_onesteplr_0.01_linearity_0.00/best_model.pt
-
+    -verbose \
+    -model_dir train \
+    -N 5 \
+    -K 1 \
+    -embedding_dir /data/embeddings/ \
+    -dataset miniImageNet \
+    -exp_name leo_onesteplr_0.01_linearity_0.00 \
+    -load train_model/miniImageNet_1shot/leo_onesteplr_0.01_linearity_0.00/best_model.pt
+echo "leo_onesteplr_0.01_linearity_0.00"
 CUDA_VISIBLE_DEVICES=0 python3 main.py -test \
-        -verbose \
-        -model_dir train \
-        -N 5 \
-        -K 1 \
-        -embedding_dir /data/embeddings/ \
-        -dataset miniImageNet \
-        -exp_name leo_onesteplr_0.01_linearity_0.01 \
-        -load train_model/miniImageNet_1shot/leo_onesteplr_0.01_linearity_0.01/best_model.pt
-
+    -verbose \
+    -model_dir train \
+    -N 5 \
+    -K 1 \
+    -embedding_dir /data/embeddings/ \
+    -dataset miniImageNet \
+    -exp_name leo_onesteplr_0.01_linearity_0.01 \
+    -load train_model/miniImageNet_1shot/leo_onesteplr_0.01_linearity_0.01/best_model.pt
+echo "leo_onesteplr_0.01_linearity_0.01"
